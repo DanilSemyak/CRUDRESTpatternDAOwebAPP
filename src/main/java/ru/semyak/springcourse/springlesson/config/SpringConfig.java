558e97eee8966772ad799f3e96677513122d843a
Danil Semenchin <semen4in.d@gmail.com>
2024-03-26 16:04:30 +0300

    Пакетное обновление + в базе id генерируется автоматиески

--- src/main/java/ru/semyak/springcourse/springlesson/config/SpringConfig.java
+++ src/main/java/ru/semyak/springcourse/springlesson/config/SpringConfig.java
@@ -1,21 +1,44 @@
 package ru.semyak.springcourse.springlesson.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+//@PropertySource("classpath:application.properties")
 public class SpringConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public SpringConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     public DataSource dataSource() {
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+//        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+//        dataSource.setUrl(environment.getProperty("url"));
+//        dataSource.setUsername(environment.getProperty("username"));
+//        dataSource.setPassword(environment.getProperty("password"));
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/first_db");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
+
+
 
         return dataSource;
     }
--- src/main/java/ru/semyak/springcourse/springlesson/dao/PersonDAO.java
+++ src/main/java/ru/semyak/springcourse/springlesson/dao/PersonDAO.java
@@ -1,11 +1,15 @@
 package ru.semyak.springcourse.springlesson.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.semyak.springcourse.springlesson.models.Person;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -28,7 +32,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES (1, ?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
@@ -38,5 +42,50 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    ///////////////////////////////////////////////////////
+    //// Тестируем производительность пакетной вставки ////
+    ///////////////////////////////////////////////////////
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES (?, ?, ?, ?)", person.getId(), person.getName(),
+                    person.getAge(), person.getEmail());
+        }
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after-before));
+    }
+    public void testBatchUpdate() {
+        List<Person> people = create1000people();
+
+        long before = System.currentTimeMillis();
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?,?,?,?)", new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                preparedStatement.setInt(1, people.get(i).getId());
+                preparedStatement.setString(2, people.get(i).getName());
+                preparedStatement.setInt(3, people.get(i).getAge());
+                preparedStatement.setString(4, people.get(i).getEmail());
+            }
+
+            @Override
+            public int getBatchSize() {
+                return people.size();
+            }
+        });
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after-before));
+    }
+
+    private List<Person> create1000people() {
+        List<Person> people = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru"));
+        }
+        return people;
     }
 }
